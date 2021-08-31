d174b3ca75c5000b6f7e26f0147f611849750279
ian-1 <iandavidwalton@mail.com>
2021-08-31 15:08:24 +0100

    Feature Test: Index shows Twitter name

---
+++ src/test/java/tech/makers/twitter/ShowsTwitterNameFeatureTest.java
@@ -0,0 +1,32 @@
+package tech.makers.twitter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriverBuilder;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ShowsTwitterNameFeatureTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setup(WebApplicationContext context) {
+        driver = MockMvcHtmlUnitDriverBuilder
+                .webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void tweetIndexShouldShowTwitterName() throws Exception {
+        driver.get("http://localhost:9990/");
+        assertThat(driver.findElement(By.tagName("body")).getText()).contains("Twitter");
+    }
+
+}
--- src/test/java/tech/makers/twitter/TweetTest.java
+++ src/test/java/tech/makers/twitter/TweetTest.java
@@ -3,13 +3,14 @@ package tech.makers.twitter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TweetTest {
     @Test
     void testConstructs() {
         Tweet subject = new Tweet("Hello, world!");
         assertEquals("Hello, world!", subject.getBody());
-        assertEquals(null, subject.getId());
+        assertNull(subject.getId());
     }
 
     @Test
