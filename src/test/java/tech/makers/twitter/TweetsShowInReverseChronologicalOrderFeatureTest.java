4730438e19dd4b4b332ca1485cd144bfd0437b80
ian-1 <iandavidwalton@mail.com>
2021-09-03 11:44:10 +0100

    Feature Test (not yet passing): shows tweets in reverse chronological order

--- src/test/java/tech/makers/twitter/TweetsShowInReverseChronologicalOrderFeatureTest.java
+++ src/test/java/tech/makers/twitter/TweetsShowInReverseChronologicalOrderFeatureTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-public class ShowsTwitterNameFeatureTest {
+public class TweetsShowInReverseChronologicalOrderFeatureTest {
     WebDriver driver;
 
     @BeforeEach
@@ -24,8 +24,16 @@ public class ShowsTwitterNameFeatureTest {
     }
 
     @Test
-    public void tweetIndexShouldShowTwitterName() throws Exception {
+    public void showsTweetsInReverseChronologicalOrder() throws Exception {
         driver.get("http://localhost:9990/");
-        assertThat(driver.findElement(By.tagName("body")).getText()).contains("Twitter");
+
+        driver.findElement(By.id("tweet-form-body")).sendKeys("Message one");
+        driver.findElement(By.id("tweet-form-submit")).click();
+        driver.findElement(By.id("tweet-form-body")).sendKeys("Message two");
+        driver.findElement(By.id("tweet-form-submit")).click();
+
+        String bodyText = driver.findElement(By.tagName("body")).getText();
+        String expectedContent = "Message two\n" + "Message one";
+        assertThat(bodyText).contains(expectedContent);
     }
 }
